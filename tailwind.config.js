f70abad5e82cf172749ec7ac11fd222bdad22b9a
Aslak Hollund <aslakhol@gmail.com>
2022-07-05 22:37:28 +0200

    trying out daisy

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,5 +3,8 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("daisyui"), require("@tailwindcss/typography")],
+  daisyui: {
+    themes: ["winter", "night", "cmyk"],
+  },
 };
