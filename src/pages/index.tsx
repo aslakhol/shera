8ba7a740318ce77375581036b5956e3677ac3bb0
Aslak Hollund <aslakhol@gmail.com>
2022-07-09 22:51:30 +0200

    style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,23 +10,28 @@ const Home: NextPage = () => {
         <meta name="description" content="Welcome page for Shera" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="flex flex-col items-center justify-center min-h-screen mx-auto pt-2 w-8/12 md:w-1/2 prose">
-        <h1 className="font-extrabold text-7xl mb-0">Shera</h1>
+      <div className="flex flex-col items-center justify-center min-h-screen mx-auto pt-2 w-8/12 md:w-1/2 gap-3">
+        <h1 className="font-extrabold text-7xl ">Shera</h1>
 
-        <p>
+        <div className="py-2" />
+
+        <p className="leading-7">
           Shera is a tool for managing events without being tied to one social
           network.
         </p>
-        <p>
+
+        <p className="leading-7">
           Do you need to invite people to an event, but they are spread across
           multiple messaging services or social networks? Then Shera is for you.
         </p>
-        <p>
+        <p className="leading-7">
           You can keep your attendees updated on your event on their terms. They
           decide how they get their information, without needing to create
           accounts or download an app.
         </p>
-        <p>Log in to create an event, and invite your friends to join.</p>
+        <p className="leading-7">
+          Log in to create an event, and invite your friends to join.
+        </p>
 
         <div className="py-2" />
 
