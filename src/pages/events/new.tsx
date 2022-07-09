76b7ecb69f584bc14b9d8d3efb368171e7467f3e
Aslak Hollund <aslakhol@gmail.com>
2022-07-09 20:01:26 +0200

    welcome page

--- src/pages/events/new.tsx
+++ src/pages/events/new.tsx
@@ -1,8 +1,19 @@
 import { NextPage } from "next";
+import Head from "next/head";
 import NewEvent from "../../features/newEvent/components/NewEvent";
 
 const NewEventPage: NextPage = () => {
-  return <NewEvent />;
+  return (
+    <>
+      <Head>
+        <title>New event</title>
+        <meta name="description" content="Page for creating new events" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <NewEvent />
+    </>
+  );
 };
 
 export default NewEventPage;
