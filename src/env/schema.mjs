5ece532b95d7fb7b94afddd5f79d59dc4f282c57
Aslak Hollund <aslakhol@gmail.com>
2022-09-07 22:55:00 +0200

    make sure email from is an email

--- src/env/schema.mjs
+++ src/env/schema.mjs
@@ -15,7 +15,7 @@ export const serverSchema = z.object({
   GOOGLE_CLIENT_ID: z.string(),
   GOOGLE_CLIENT_SECRET: z.string(),
   SENDGRID_API_KEY: z.string(),
-  EMAIL_FROM: z.string(),
+  EMAIL_FROM: z.string().email(),
 });
 
 /**
