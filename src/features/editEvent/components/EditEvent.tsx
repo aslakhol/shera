eccab86198894b7060b907f3f99f42bf43f04bc2
Aslak Hollund <aslakhol@gmail.com>
2022-10-02 00:57:42 +0200

    get edit event ready for reusing the form

--- src/features/editEvent/components/EditEvent.tsx
+++ src/features/editEvent/components/EditEvent.tsx
@@ -1,10 +1,17 @@
+import { CreateEventSchemaType } from "@/features/newEvent/formValidation";
 import { trpc } from "@/utils/trpc";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import { useUpdateEvent } from "../hooks/useUpdateEvent";
 import EditEventForm from "./EditEventForm";
 
 type EditEventProps = { eventId: string };
 
 const EditEvent = (props: EditEventProps) => {
   const { eventId } = props;
+  const { data: session, status } = useSession();
+  const router = useRouter();
+  const updateEventMutation = useUpdateEvent();
 
   const {
     data: event,
@@ -21,16 +28,42 @@ const EditEvent = (props: EditEventProps) => {
     return <div className="h-screen">{error.message}</div>;
   }
 
-  if (!isSuccess) {
+  if (!isSuccess || status !== "authenticated") {
     return <div className="h-screen"></div>;
   }
+
+  if (!session.user.id) {
+    return <>Not logged in</>;
+  }
+
+  if (session.user.id !== event.host.id) {
+    return <>You are not the host of this event</>;
+  }
+
+  const handleSubmit = (
+    values: CreateEventSchemaType,
+    successActions: () => void
+  ) => {
+    updateEventMutation.mutate(
+      {
+        ...values,
+        eventId: event.eventId,
+      },
+      {
+        onSuccess: (result) => {
+          router.push(`/events/${result.event.eventId}`);
+          successActions();
+        },
+      }
+    );
+  };
 
   return (
     <div className="flex flex-col items-center min-h-screen-content mx-auto py-8 w-10/12 md:w-1/2">
       <div className="prose">
         <h1 className="py-2 text-center">Edit {event.title}</h1>
       </div>
-      <EditEventForm event={event} />
+      <EditEventForm event={event} submit={handleSubmit} />
     </div>
   );
 };
--- src/features/editEvent/components/EditEventForm.tsx
+++ src/features/editEvent/components/EditEventForm.tsx
@@ -2,64 +2,36 @@ import Spinner from "@/components/Spinner";
 import Textarea from "@/components/Textarea";
 import TextInput from "@/components/TextInput";
 import Title from "@/features/newEvent/components/Title";
-import { createEventSchema } from "@/features/newEvent/formValidation";
+import {
+  createEventSchema,
+  CreateEventSchemaType,
+} from "@/features/newEvent/formValidation";
 import { useZodForm } from "@/utils/zodForm";
 import { Events, User } from "@prisma/client";
-import { useSession } from "next-auth/react";
-import { useRouter } from "next/router";
 import { useUpdateEvent } from "../hooks/useUpdateEvent";
 
 type EditEventFormProps = {
-  event: Events & {
+  event?: Events & {
     host: User;
   };
+  submit: (values: CreateEventSchemaType, successActions: () => void) => void;
 };
 
 const EditEventForm = (props: EditEventFormProps) => {
-  const { event } = props;
-  const router = useRouter();
+  const { event, submit } = props;
   const updateEventMutation = useUpdateEvent();
 
   const methods = useZodForm({
     schema: createEventSchema,
     defaultValues: {
-      title: event.title,
-      time: event.time,
-      description: event.description,
-      place: event.place || undefined,
+      ...event,
+      place: event?.place || undefined,
     },
   });
 
-  const { data: session } = useSession();
-
-  if (session === undefined) {
-    return <></>;
-  }
-
-  if (session === null || !session.user?.id) {
-    return <>Not logged in</>;
-  }
-
-  if (session.user.id !== event.host.id) {
-    return <>You are not the host of this event</>;
-  }
-
   return (
     <form
-      onSubmit={methods.handleSubmit(async (values) => {
-        updateEventMutation.mutate(
-          {
-            ...values,
-            eventId: event.eventId,
-          },
-          {
-            onSuccess: (result) => {
-              methods.reset();
-              router.push(`/events/${result.event.eventId}`);
-            },
-          }
-        );
-      })}
+      onSubmit={methods.handleSubmit((values) => submit(values, methods.reset))}
       className={`form-control w-full max-w-xs gap-2`}
     >
       <Title methods={methods} />
