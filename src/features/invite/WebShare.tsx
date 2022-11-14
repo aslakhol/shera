ad97c3b3d57dece8d7cf3b3dfe0df2def0960bd4
Aslak Hollund <aslakhol@gmail.com>
2022-11-14 10:39:32 +0100

    fix null in invite

--- src/features/invite/WebShare.tsx
+++ src/features/invite/WebShare.tsx
@@ -1,5 +1,6 @@
 import { Events } from "@prisma/client";
 import { User } from "next-auth";
+import { useSession } from "next-auth/react";
 
 type WebShareProps = {
   event: Events & {
@@ -10,11 +11,17 @@ type WebShareProps = {
 const WebShare = (props: WebShareProps) => {
   const { event } = props;
 
+  const { data } = useSession();
+
+  const text = data?.user.name
+    ? `${data.user.name} has invited you to ${event.title}!`
+    : `You have been invited to ${event.title}!`;
+
   const share = () => {
     navigator
       .share({
         title: "You've been invited to an event!",
-        text: `${event.host.name} has invited you to ${event.title}!`,
+        text: text,
         url: `https://shera.no/events/${event.eventId}`,
       })
       .then(() => console.log("Successful share"))
