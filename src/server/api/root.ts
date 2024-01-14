1604fa84e4c3eb63554d38abe221ea695d2bf92b
Aslak Hollund <aslakhol@gmail.com>
2024-01-14 21:46:05 +0100

    port user router

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,7 @@
-import { postRouter } from "~/server/api/routers/post";
 import { createTRPCRouter } from "~/server/api/trpc";
+import { postsRouter } from "./routers/posts";
+import { usersRouter } from "./routers/users";
+import { eventsRouter } from "./routers/events";
 
 /**
  * This is the primary router for your server.
@@ -7,7 +9,9 @@ import { createTRPCRouter } from "~/server/api/trpc";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  post: postRouter,
+  posts: postsRouter,
+  users: usersRouter,
+  events: eventsRouter,
 });
 
 // export type definition of API
