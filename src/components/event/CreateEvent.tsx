1433405311a2c3378d419571c38d1d88c1109ca0
Aslak Hollund <aslakhol@gmail.com>
2024-02-03 13:10:35 +0100

    redirect to event after creation

--- src/components/event/CreateEvent.tsx
+++ src/components/event/CreateEvent.tsx
@@ -3,12 +3,15 @@ import { api } from "../../utils/api";
 import { type EventSchemaType } from "../../utils/formValidation";
 import { EventForm } from "../EventForm";
 import { toast } from "sonner";
+import { useRouter } from "next/router";
 
 export const CreateEvent = () => {
   const session = useSession();
+  const router = useRouter();
   const createEventMutation = api.events.createEvent.useMutation({
     onSuccess: (response) => {
       toast.success(`${response.event.title} created!`);
+      void router.push(`/events/${response.event.eventId}`);
     },
     onError: (error) => {
       toast.error(error.message);
@@ -25,7 +28,3 @@ export const CreateEvent = () => {
 
   return <EventForm onSubmit={createEvent} />;
 };
-
-// Make event page
-// Redirect to event page
-// Edit event
