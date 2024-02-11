997636136199f9fca17acc02382f5564a885bb58
Aslak Hollund <aslakhol@gmail.com>
2024-02-11 18:34:03 +0100

    remove old directory

--- old/src/features/post/formValidation.ts
+++
@@ -1,7 +0,0 @@
-import { z } from "zod";
-
-export type PostSchemaType = z.infer<typeof postSchema>;
-
-export const postSchema = z.object({
-  message: z.string(),
-});
--- old/src/utils/zStringToNumber.ts
+++
@@ -1,3 +0,0 @@
-import { z } from "zod";
-
-export const zStringToNumber = z.string().transform(Number);
