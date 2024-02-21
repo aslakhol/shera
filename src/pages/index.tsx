85ec7fdbf342afdda00f161f4b976f5ff1e6b82b
Aslak Hollund <aslakhol@gmail.com>
2024-02-21 22:20:41 +0100

    redirect to events if logged in

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,16 @@
+import { useSession } from "next-auth/react";
 import { Welcome } from "../components/welcome/Welcome";
 import { type NextPageWithLayout } from "./_app";
+import { useRouter } from "next/router";
 
 const Home: NextPageWithLayout = () => {
+  const session = useSession();
+  const router = useRouter();
+
+  if (session.status === "authenticated") {
+    void router.push("/events");
+  }
+
   return <Welcome />;
 };
 
