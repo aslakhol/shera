ee3763685c2379ccbaf4f7e697facf45df52c918
Aslak Hollund <aslakhol@gmail.com>
2024-02-23 22:04:24 +0100

    set up basic ssg

--- src/pages/events/[eventId]/index.tsx
+++ src/pages/events/[eventId]/index.tsx
@@ -4,6 +4,11 @@ import { useRouter } from "next/router";
 import { type NextPageWithLayout } from "../../_app";
 import { type ReactElement } from "react";
 import { MainLayout } from "../../../components/Layout";
+import { createServerSideHelpers } from "@trpc/react-query/server";
+import { createInnerTRPCContext } from "../../../server/api/trpc";
+import { appRouter } from "../../../server/api/root";
+import superjson from "superjson";
+import { type GetStaticPaths, type GetStaticProps } from "next";
 
 const EventPage: NextPageWithLayout = () => {
   const { query } = useRouter();
@@ -33,3 +38,35 @@ EventPage.getLayout = function getLayout(page: ReactElement) {
 };
 
 export default EventPage;
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [
+      {
+        params: {
+          eventId: "1",
+        },
+      },
+    ],
+    fallback: "blocking",
+  };
+};
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const helpers = createServerSideHelpers({
+    router: appRouter,
+    ctx: createInnerTRPCContext({ session: null }),
+    transformer: superjson,
+  });
+
+  await helpers.events.event.prefetch({
+    eventId: Number(context.params?.eventId),
+  });
+
+  return {
+    props: {
+      trpcState: helpers.dehydrate(),
+    },
+    revalidate: 60 * 15,
+  };
+};
