c511b7d50682e92e6e982244be22079c328a4f6b
Aslak Hollund <aslakhol@gmail.com>
2024-02-25 22:48:45 +0100

    remove the old migrations again

--- prisma/foo/20220717192028_optional_host_on_events/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "Events" ADD COLUMN     "hostId" TEXT;
-
--- AddForeignKey
-ALTER TABLE "Events" ADD CONSTRAINT "Events_hostId_fkey" FOREIGN KEY ("hostId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
