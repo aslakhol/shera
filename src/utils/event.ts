812797d1641078c012617584dcd4e15f60b659cf
Aslak Hollund <aslakhol@gmail.com>
2024-02-27 19:37:15 +0100

    function to slugify events

---
+++ src/utils/event.ts
@@ -0,0 +1,22 @@
+const slugStringMaxLength = 35;
+
+export const slugifyEvent = (event: { title: string }) => {
+  const slug = event.title
+    .toLowerCase()
+    .replace(/ /g, "-")
+    .replace(/[^\w-]+/g, "")
+    .replace(/-{2,}/g, "-");
+
+  if (slug.length <= slugStringMaxLength) {
+    return slug;
+  }
+
+  const lastDashWithinLimit = slug
+    .substring(0, slugStringMaxLength)
+    .lastIndexOf("-");
+  if (lastDashWithinLimit > 0) {
+    return slug.substring(0, lastDashWithinLimit);
+  }
+
+  return slug.substring(0, slugStringMaxLength);
+};
