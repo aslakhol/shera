1bd2412820d48d262e9addb2181a7914fc9ef0e4
Aslak Hollund <aslakhol@gmail.com>
2024-05-12 22:06:48 +0200

    remove unused import

--- src/pages/events/index.tsx
+++ src/pages/events/index.tsx
@@ -1,6 +1,5 @@
 import Head from "next/head";
 import { MyEvents } from "../../components/myEvents/MyEvents";
-import { useSession } from "next-auth/react";
 import { type NextPageWithLayout } from "../_app";
 import { MainLayout } from "../../components/Layout";
 import { type ReactElement } from "react";
