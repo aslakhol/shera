f5f50d39a5ea2f219f7624cb5f86b1454f3c4964
Aslak Hollund <aslakhol@gmail.com>
2024-05-18 17:38:00 +0200

    make sortheader

--- src/components/admin/events/Columns.tsx
+++ src/components/admin/events/Columns.tsx
@@ -1,5 +1,5 @@
 import { type Attendee, type User, type Event } from "@prisma/client";
-import { type ColumnDef } from "@tanstack/react-table";
+import { type Column, type ColumnDef } from "@tanstack/react-table";
 import { format } from "date-fns";
 import Link from "next/link";
 import { fullEventId } from "../../../utils/event";
@@ -11,23 +11,7 @@ export const columns: ColumnDef<
 >[] = [
   {
     accessorKey: "dateTime",
-    header: ({ column }) => {
-      return (
-        <Button
-          variant="ghost"
-          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
-        >
-          Date
-          {column.getIsSorted() === "asc" && (
-            <ArrowUp className="ml-2 h-4 w-4" />
-          )}
-          {column.getIsSorted() === "desc" && (
-            <ArrowDown className="ml-2 h-4 w-4" />
-          )}
-        </Button>
-      );
-    },
-
+    header: ({ column }) => <SortHeader headerTitle="Date" column={column} />,
     accessorFn: (row) => format(row.dateTime, "LLLL do, H:mm"),
   },
   {
@@ -83,3 +67,23 @@ export const columns: ColumnDef<
     },
   },
 ];
+
+type SortHeaderProps = {
+  headerTitle: string;
+  column: Column<Event & { host: User; attendees: Attendee[] }>;
+};
+
+const SortHeader = ({ headerTitle, column }: SortHeaderProps) => {
+  return (
+    <Button
+      variant="ghost"
+      onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+    >
+      {headerTitle}
+      {column.getIsSorted() === "asc" && <ArrowUp className="ml-2 h-4 w-4" />}
+      {column.getIsSorted() === "desc" && (
+        <ArrowDown className="ml-2 h-4 w-4" />
+      )}
+    </Button>
+  );
+};
