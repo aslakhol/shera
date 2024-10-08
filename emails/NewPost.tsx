201883be4443b8c69a44746a309c610e7b2d2cca
Aslak Hollund <aslakhol@gmail.com>
2024-10-08 21:31:08 +0200

    new post email

---
+++ emails/NewPost.tsx
@@ -0,0 +1,29 @@
+import * as React from "react";
+import { type Attendee, type User, type Event } from "@prisma/client";
+import { EventEmail } from "./components/EventEmail";
+import { previewEvent, previewUser } from "./previews";
+
+type NewPostProps = {
+  event: Event & { host: User; attendees: Attendee[] };
+  poster: User;
+};
+
+export const NewPost = ({ event, poster }: NewPostProps) => {
+  const body = `${poster.name ?? "Someone"} posted on ${event.title}`;
+
+  return (
+    <EventEmail
+      event={event}
+      previewText={`New post on ${event.title}`}
+      aboveText={`New post on`}
+      bodyText={body}
+    />
+  );
+};
+
+export default NewPost;
+
+NewPost.PreviewProps = {
+  event: previewEvent,
+  poster: previewUser,
+} satisfies NewPostProps;
