9680f2870d9d065ba3b0f2da1432a0ef6a6508cd
Aslak Hollund <aslakhol@gmail.com>
2024-10-10 13:29:25 +0200

    increase description length to 10 000

--- src/utils/formValidation.ts
+++ src/utils/formValidation.ts
@@ -27,7 +27,7 @@ export type EventSchemaType = z.infer<typeof eventSchema>;
 
 export const eventSchema = z.object({
   title: z.string().min(3).max(100),
-  description: z.string().min(3).max(1000),
+  description: z.string().min(3).max(10000),
   dateTime: z.date(),
   timeZone: z.string(),
   place: z.string().optional(),
