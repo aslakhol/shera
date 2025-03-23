7a85b56803f7365b86e179cdb5d083bde02871d1
Aslak Hollund <aslakhol@gmail.com>
2025-03-23 21:56:00 +0100

    only display categories when we have content

--- src/components/myEvents/MyEvents.tsx
+++ src/components/myEvents/MyEvents.tsx
@@ -52,11 +52,11 @@ export const MyEvents = () => {
         </div>
       )}
 
-      <h2>Upcoming</h2>
+      {events?.upcoming && events.upcoming.length > 0 && <h2>Upcoming</h2>}
       {events?.upcoming.map((event) => (
         <EventRow event={event.event} key={`event-${event.event.publicId}`} />
       ))}
-      <h2>Finished</h2>
+      {events?.finished && events.finished.length > 0 && <h2>Finished</h2>}
       {events?.finished.map((event) => (
         <EventRow event={event.event} key={`event-${event.event.publicId}`} />
       ))}
