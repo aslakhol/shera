42f28e35c42f4ea366c93363dde27eca3bf0f510
Aslak Hollund <aslakhol@gmail.com>
2025-03-23 19:20:49 +0100

    make a wrap links component

--- src/components/event/Body.tsx
+++ src/components/event/Body.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 type Props = { description: string };
 
 export const Body = ({ description }: Props) => {
@@ -6,8 +8,55 @@ export const Body = ({ description }: Props) => {
   return (
     <div className="flex flex-col gap-1">
       {paragraphs.map((paragraph, index) => (
-        <p className="break-all" key={`event-body-paragraph-${index}`}>{paragraph}</p>
+        <p className="break-all" key={`event-body-paragraph-${index}`}>
+          <WrapLinks text={paragraph} />
+        </p>
       ))}
     </div>
   );
+};
+
+type WrapLinksProps = { text: string };
+
+const WrapLinks = ({ text }: WrapLinksProps) => {
+  const parts = text.split(/\s+/g);
+
+  return (
+    <span>
+      {parts.map((part, index) => {
+        if (part.startsWith("http")) {
+          const url = safeParseUrl(part);
+          if (!url) {
+            return <span key={`wrap-link-${index}`}> {part} </span>;
+          }
+
+          return (
+            <Link
+              className="hover:underline"
+              key={`wrap-link-${index}`}
+              rel="noopener noreferrer"
+              target="_blank"
+              href={url.href}
+            >
+              {part}
+            </Link>
+          );
+        }
+
+        return <span key={`wrap-link-${index}`}> {part} </span>;
+      })}
+    </span>
+  );
+};
+
+const safeParseUrl = (url: string) => {
+  if (!url.startsWith("http")) {
+    return null;
+  }
+
+  try {
+    return new URL(encodeURI(url));
+  } catch (error) {
+    return null;
+  }
 };
