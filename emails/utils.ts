88ae645ae15f3de7f013d4cfb6df25a3e73abfc6
Aslak Hollund <aslakhol@gmail.com>
2025-03-30 16:18:30 +0200

    move the util for formatting names in the event email back into the utils function

--- emails/utils.ts
+++ emails/utils.ts
@@ -13,3 +13,21 @@ export const formatHostNamesShort = (hosts: Array<{ name: string | null }>) => {
 
   return `the hosts`;
 };
+
+export const infoBoxFormatHostNames = (
+  hosts: Array<{ name: string | null }>,
+) => {
+  const namesWithValues = hosts
+    .filter((h) => h.name !== null)
+    .map((h) => h.name!);
+
+  if (namesWithValues.length === 0) {
+    return "The host";
+  }
+
+  if (namesWithValues.length <= 3) {
+    return namesWithValues.join(", ").replace(/, ([^,]*)$/, " and $1");
+  }
+
+  return `${namesWithValues[0]} and ${namesWithValues.length - 1} others`;
+};
